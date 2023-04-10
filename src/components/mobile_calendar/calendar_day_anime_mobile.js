f335c64b26045045560157c93e5558d43a4653c9
Josh Martin <josh.martin@visiblebody.com>
2023-04-10 19:26:00 -0400

    fix issues with commit

--- src/components/mobile_calendar/calendar_day_anime_mobile.js
+++ src/components/mobile_calendar/calendar_day_anime_mobile.js
@@ -2,12 +2,8 @@ import React, { Fragment, useState } from "react";
 import PopoverAnime from '../calendar/popover_anime';
 
 export default function CalendarDayAnimeMobile({ day, formattedArray }) {
-    const [isExpanded, setIsExpanded] = useState(false);
+    // const [isExpanded, setIsExpanded] = useState(false);
     const [popoverVisibility, setPopoverVisibility] = useState({});
-
-    const toggleExpand = () => {
-        setIsExpanded(!isExpanded);
-    };
 
     const handlePopoverToggle = (popoverId) => {
         setPopoverVisibility((prevState) => ({
